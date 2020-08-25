2301f6998c89e1452566c128872d4ededfdb7bbe
Eduardo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-08-24 23:48:13 -0600

    Modify to UrlFields

--- eventup/events/models/events.py
+++ eventup/events/models/events.py
@@ -9,13 +9,16 @@ class Event(models.Model):
     # Id
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-    # Event Relations
-
     # Event data
     name = models.CharField(max_length=100, unique=True)
     date = models.DateField()
     hour = models.TimeField()
     description = models.TextField()
-    url = models.CharField(max_length=300)
-    banner = models.CharField(max_length=300)
-    logo = models.CharField(max_length=300)
+    url = models.URLField()
+    banner = models.URLField()
+    logo = models.URLField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    # Event Relations
