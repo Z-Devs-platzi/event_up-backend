38aff2fb5c22bd860e7cccd877a2e70859d08d79
Eduardo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-08-24 21:15:22 -0600

    Instaling events app

--- config/settings/base.py
+++ config/settings/base.py
@@ -50,6 +50,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     'eventup.users.apps.UsersAppConfig',
+    'eventup.events.apps.EventAppConfig'
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
