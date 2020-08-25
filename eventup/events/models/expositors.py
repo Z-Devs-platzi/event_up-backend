0c4ab9ef7d25adc9e01e9ab2ebfe4941d486a4c2
Eduardo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-08-24 23:49:00 -0600

    expositors models

---
+++ eventup/events/models/expositors.py
@@ -0,0 +1,20 @@
+''' Expositors Model '''
+
+import uuid
+from django.db import models
+
+
+class Expositor(models.Model):
+    ''' Expositor Model '''
+
+    # Id
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # Expositor data
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    bio = models.TextField()
+    twitter = models.URLField()
+    profile_picture = models.URLField()
+
+    # Expositors Relations
