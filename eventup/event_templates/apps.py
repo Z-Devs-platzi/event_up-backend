419efed590ba9dae45102511454d6fc415d39ea9
Eduardo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-08-26 11:59:38 -0600

    Templates app config!

---
+++ eventup/event_templates/apps.py
@@ -0,0 +1,10 @@
+""" Templates App """
+
+from django.apps import AppConfig
+
+
+class TemplateAppConfig(AppConfig):
+    """ Template app config """
+
+    name = "eventup.events"
+    verbose_name = "events"
