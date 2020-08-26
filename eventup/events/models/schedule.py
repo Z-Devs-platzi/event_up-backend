516f6a2d737777d942878c09afc552ab1420f93f
Eduardo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-08-26 10:12:46 -0600

    Fixing schedule model

--- eventup/events/models/schedule.py
+++ eventup/events/models/schedule.py
@@ -8,8 +8,12 @@ from eventup.utils.models import GeneralModel
 
 class Schedule(GeneralModel):
 
+    # Schedule data
+    title = models.CharField(max_length=300)
     date = models.DateField()
+    hour = models.TimeField()
 
+    # Relations
     expositors = models.ManyToManyField(
         to="Expositor"
     )
