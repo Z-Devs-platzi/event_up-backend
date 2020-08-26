d407845739de31ce144b61600c3d23b8c85bfa4f
Eduardo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-08-26 15:05:03 -0600

    Communities admin config

--- eventup/communities/admin.py
+++ eventup/communities/admin.py
@@ -4,36 +4,13 @@
 from django.contrib import admin
 
 # Models
-from eventup.events.models import Event, Sponsor, Expositor, Schedule
+from eventup.communities.models import Community
 
 
-@admin.register(Event)
-class EventAdmin(admin.ModelAdmin):
+@admin.register(Community)
+class CommunityAdmin(admin.ModelAdmin):
     ''' Event model admin '''
 
-    list_display = ('name', 'date', 'hour', 'url')
+    list_display = ('name', 'social_url')
     search_fields = ('name',)
-    list_filter = ('name', 'date', 'hour')
-
-
-@admin.register(Sponsor)
-class SponsorsAdmin(admin.ModelAdmin):
-    ''' Sponsors model admin '''
-
-    list_display = ('name', 'level')
-    search_fields = ('name',)
-    list_filter = ('level',)
-
-
-@admin.register(Expositor)
-class ExpositorAdmin(admin.ModelAdmin):
-    ''' Expositor model admin '''
-    list_display = ('first_name', 'last_name', 'twitter')
-    search_fields = ('first_name', 'last_name')
-    list_filter = ('first_name', 'last_name')
-
-
-@admin.register(Schedule)
-class ScheduleAdmin(admin.ModelAdmin):
-    ''' Schedule model admin '''
-    list_display = ('title', 'date', 'hour')
+    list_filter = ('name',)
