380f9da36739014f38783684644455499fa60bc5
Eduardo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-08-26 14:36:24 -0600

    Folder structure

---
+++ eventup/communities/admin.py
@@ -0,0 +1,39 @@
+''' Event models admin '''
+
+# Django
+from django.contrib import admin
+
+# Models
+from eventup.events.models import Event, Sponsor, Expositor, Schedule
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    ''' Event model admin '''
+
+    list_display = ('name', 'date', 'hour', 'url')
+    search_fields = ('name',)
+    list_filter = ('name', 'date', 'hour')
+
+
+@admin.register(Sponsor)
+class SponsorsAdmin(admin.ModelAdmin):
+    ''' Sponsors model admin '''
+
+    list_display = ('name', 'level')
+    search_fields = ('name',)
+    list_filter = ('level',)
+
+
+@admin.register(Expositor)
+class ExpositorAdmin(admin.ModelAdmin):
+    ''' Expositor model admin '''
+    list_display = ('first_name', 'last_name', 'twitter')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('first_name', 'last_name')
+
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    ''' Schedule model admin '''
+    list_display = ('title', 'date', 'hour')
