8b46c3f2605d97a339b5456356bdeab5878c3a7e
Miguel Ángel Torres Vargas <ingeniero.miguelvargas@gmail.com>
2020-08-27 09:17:38 -0500

    FIX Deploy

--- config/scripts/config.sh
+++ config/scripts/config.sh
@@ -2,27 +2,35 @@
 
 # Use: sh deploy.sh
 # Flags: sh deploy.sh update/clean
-
-# Config elements
-COMMAND=$1
-
+sudo chmod +x ./config.sh
+sudo chmod +x ./deploy.sh
+# PATH
 BASE_NAME=`basename $0`
 BASE_PATH=$(dirname $(readlink -f $0))
 BASE_PATH_NAME="$BASE_PATH/$BASE_NAME"
+# echo "Script path with name: $BASE_PATH_NAME"
 
-echo "Script path with name: $BASE_PATH_NAME"
+# Config elements
+COMMAND=$1 || ''
 
-# Update all
-if [ -n "$COMMAND" && $COMMAND = 'update' ]; then
-    sudo apt-get update -y && sudo apt-get upgrade -y
-fi
+# # Update all
+# if [ -n "$COMMAND" && $COMMAND = 'update' ]; then
+#     sudo apt-get update -y && sudo apt-get upgrade -y
+# fi
 
 # Permissions Git
-cd /home/ubuntu/backend/
-cd .git/objects
+ROUTE=/home/ubuntu/backend/
+cd `${ROUTE}.git/objects`
+pwd
+ls
 sudo chown -R "${USER:-$(id -un)}" .
 
 # Inside the project
-cp `$BASE_PATH/deploy.sh` /home/ubuntu/backend/
-cd /home/ubuntu/backend/
-sh deploy.sh $COMMAND
+ROUTE_FILE=`$BASE_PATH/deploy.sh`
+sudo rm -rf $ROUTE_FILE
+sudo cp $ROUTE_FILE `${ROUTE}deploy.sh`
+sudo chmod +x `${ROUTE}deploy.sh`
+cd $ROUTE
+pwd
+ls
+# sh deploy.sh $COMMAND
