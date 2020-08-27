0f8f033a6dec8fe4db2606cea9b849db39ef125c
Miguel Ángel Torres Vargas <ingeniero.miguelvargas@gmail.com>
2020-08-26 23:20:31 -0500

    FIX Deploy

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
 # Use: sh deploy.sh
-# Flags: sh deploy.sh update clean
+# Flags: sh deploy.sh update/clean
 
 # Config elements
-UPDATESYSTEM=$1
-REMOVEDATA=$2
-# ORIGIN=$3 || 'master'
+COMMAND=$1
 
 # Update all
-if [ -n "$UPDATESYSTEM" ]; then
-    if [ $UPDATESYSTEM = 'update' ]; then
+if [ -n "$COMMAND" ]; then
+    if [ $COMMAND = 'update' ]; then
         sudo apt-get update -y && sudo apt-get upgrade -y
     fi
 fi
 
 # Inside the project
 cd /home/ubuntu/backend/
+export COMPOSE_FILE=production.yml
 
 # Stop Services
 sudo supervisorctl stop eventup
-sudo docker-compose -f production.yml down
+sudo docker-compose down
 
 # Remove all
-if [ -n "$REMOVEDATA" ]; then
-    if [ "$REMOVEDATA" = 'clean' ]; then
-        # Remove images
-        sudo docker rmi -f $(sudo docker images -a -q)
-    # else
-        # # Remove all containers and images
-        # sudo docker rm -vf $(sudo docker ps -a -q)
+if [ -n "$COMMAND" ]; then
+    if [ "$COMMAND" = 'clean' ]; then
+        # Remove Data
+        sudo docker rm -vf $(sudo docker ps -a -q)
+        sudo docker rmi -f $(sudo docker images -a -q)\
+        sudo docker system prune -f
+        sudo docker volume prune -f
     fi
 fi
 
@@ -38,7 +37,18 @@ fi
 git checkout master
 git reset --hard origin/master
 git fetch --all
-git pull origin master | sudo docker-compose -f production.yml build
+git pull origin master | sudo docker-compose build
+
+# Remove all
+if [ -n "$COMMAND" ]; then
+    if [ "$COMMAND" = 'clean' ]; then
+        # Migrations
+        find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+        find . -path "*/migrations/*.pyc"  -delete
+        sudo docker-compose run --rm django python manage.py makemigrations
+        sudo docker-compose run --rm django python manage.py migrate
+    fi
+fi
 
 # Up new services and Active auto services
-sudo docker-compose -f production.yml up --force-recreate -d && sudo supervisorctl start eventup && sudo supervisorctl restart all
+sudo docker-compose up --force-recreate -d && sudo supervisorctl start eventup && sudo supervisorctl restart all
