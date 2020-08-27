055a85e49990b75ce059993cabbb11dbbc63fd60
Eduardo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-08-26 23:00:37 -0600

    fixing sponsors models

--- eventup/events/models/sponsors.py
+++ eventup/events/models/sponsors.py
@@ -11,6 +11,7 @@ class Sponsor(GeneralModel):
 
     name = models.CharField(max_length=100)
     level = models.CharField(max_length=100)
+    web = models.URLField()
     logo = models.CharField(max_length=300)
 
     def __str__(self):
