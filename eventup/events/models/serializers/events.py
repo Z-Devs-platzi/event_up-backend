7cc227dab59f696fe104f92c3160106b142cbb11
Eduardo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-08-29 09:45:59 -0600

    Event serializer

---
+++ eventup/events/models/serializers/events.py
@@ -0,0 +1,28 @@
+""" Events Serializer """
+
+# Django REST Framework
+from rest_framework import serializers
+
+# Models
+from eventup.events.models import Event
+
+
+class EventModelSerializer(serializers.ModelSerializer):
+    """ Event model serializer """
+
+    class Meta:
+        """ Meta class """
+
+        model = Event
+        fields = (
+            'name',
+            'data',
+            'description',
+            'url',
+            'banner_img',
+            'banner_title',
+            'code',
+            'template',
+            'sponsor',
+            'schedule',
+        )
