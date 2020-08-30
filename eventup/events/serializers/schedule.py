1b76cc42a2c114f2550b110ac59dfc52b5e1fb9a
Eduardo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-08-29 18:36:18 -0600

    schedule meta class

--- eventup/events/serializers/schedule.py
+++ eventup/events/serializers/schedule.py
@@ -10,10 +10,12 @@ from eventup.events.models import Schedule
 class ScheduleModelSerializer(serializers.ModelSerializer):
     """ Schedule model serializer """
 
-    model = Schedule
-    fields = (
-        'title',
-        'data',
-        'description',
-        'expostiors',
-    )
+    class Meta:
+        """ Meta class """
+        model = Schedule
+        fields = (
+            'title',
+            'data',
+            'description',
+            'expostiors',
+        )
