f62c22ddd65031c8c9d52d08660c018c067abb5c
Miguel Ángel Torres Vargas <ingeniero.miguelvargas@gmail.com>
2020-09-05 07:46:36 -0500

    FIX Update

--- eventup/utils/mixins.py
+++ eventup/utils/mixins.py
@@ -88,28 +88,32 @@ class CustomUpdateModelMixin:
     """
 
     def update(self, request, *args, **kwargs):
+        data = None
         partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        # Check if exist
+        try:
+            instance = self.get_object()
+        except Exception:
+            data = custom_actions.set_response(status.HTTP_400_BAD_REQUEST, 'Not found data')
 
-        # Validate Model
-        if not serializer.is_valid():
-            data = self.custom_actions.set_response(status.HTTP_400_BAD_REQUEST,
-                                                    'Error to make the object', serializer.errors)
-        else:
-            # Save Object
-            self.perform_update(serializer)
-
-            # Attr
-            if getattr(instance, '_prefetched_objects_cache', None):
-                # If 'prefetch_related' has been applied to a queryset, we need to
-                # forcibly invalidate the prefetch cache on the instance.
-                instance._prefetched_objects_cache = {}
-            # Return User
-            content = serializer.data
-
-            # Return Data
-            data = custom_actions.set_response(status.HTTP_200_OK, 'Get information!', content)
+        if not data:
+            serializer = self.get_serializer(instance, data=request.data, partial=partial)
+            # Validate Model
+            if not serializer.is_valid():
+                data = self.custom_actions.set_response(status.HTTP_400_BAD_REQUEST,
+                                                        'Error to make the object', serializer.errors)
+            else:
+                # Save Object
+                self.perform_update(serializer)
+                # Attr
+                if getattr(instance, '_prefetched_objects_cache', None):
+                    # If 'prefetch_related' has been applied to a queryset, we need to
+                    # forcibly invalidate the prefetch cache on the instance.
+                    instance._prefetched_objects_cache = {}
+                # Return User
+                content = serializer.data
+                # Return Data
+                data = custom_actions.set_response(status.HTTP_200_OK, 'Get information!', content)
         # Get Status
         return custom_actions.custom_response(data)
 
